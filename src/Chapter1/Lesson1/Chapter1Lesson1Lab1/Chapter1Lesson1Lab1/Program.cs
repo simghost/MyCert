f41e281e47d9d22fa7a3c76753b27e32db082029
robert davis <rad316@comcast.net>
2012-07-09 11:35:23 -0400

    Added Chapter 1 Lesson 1 Lab 1

---
+++ src/Chapter1/Lesson1/Chapter1Lesson1Lab1/Chapter1Lesson1Lab1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter1Lesson1Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Tony", "DeMarco", 53);
+
+            Console.WriteLine(person1.ToString());
+
+            Console.ReadLine();
+        }
+    }
+
+    struct Person
+    {
+        public string _firstName;
+        public string _lastName;
+        int _age;
+
+
+        public Person(string firstName, string lastName, int age)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}, Age: {2}", _firstName, _lastName, _age);
+        }
+    }
+}
