d45ddae9b69f3e843475246cf97437d3f94b7a47
robert davis <rad316@comcast.net>
2012-07-09 11:41:42 -0400

    Added code for Ch1 Lesson 1 Exercise 1
    Added Gender enumeration

--- src/Chapter1/Lesson1/Chapter1Lesson1Lab1/Chapter1Lesson1Lab1/Program.cs
+++ src/Chapter1/Lesson1/Chapter1Lesson1Lab1/Chapter1Lesson1Lab1/Program.cs
@@ -9,7 +9,7 @@ namespace Chapter1Lesson1Lab1
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person("Tony", "DeMarco", 53);
+            Person person1 = new Person("Tony", "DeMarco", 53,Person.Gender.Male);
 
             Console.WriteLine(person1.ToString());
 
@@ -19,21 +19,24 @@ namespace Chapter1Lesson1Lab1
 
     struct Person
     {
+        public enum Gender { Male, Felmale };
         public string _firstName;
         public string _lastName;
         int _age;
+        Gender _gender;
 
 
-        public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age, Gender gender)
         {
             _firstName = firstName;
             _lastName = lastName;
             _age = age;
+            _gender = gender;
         }
 
         public override string ToString()
         {
-            return string.Format("{0} {1}, Age: {2}", _firstName, _lastName, _age);
+            return string.Format("{0} {1}, Age: {2}  Gender: {3}", _firstName, _lastName, _age, _gender);
         }
     }
 }
