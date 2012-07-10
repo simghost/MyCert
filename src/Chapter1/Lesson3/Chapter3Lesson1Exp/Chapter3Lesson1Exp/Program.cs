f6a9f26338ebf2d78bbeb0eb65989567932cc085
robert davis <rad316@comcast.net>
2012-07-10 11:04:04 -0400

    Added Chapter 1 Lesson3 expirements code

---
+++ src/Chapter1/Lesson3/Chapter3Lesson1Exp/Chapter3Lesson1Exp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter3Lesson1Exp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new DerivedException();
+            }
+            catch ( Exception ex)
+            {
+
+                Console.WriteLine("{0} : {1}",ex.Source,ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class DerivedException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "An error occured in the application";
+            }
+        }
+    }
+
+    class BigClass : IDisposable
+    {
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
