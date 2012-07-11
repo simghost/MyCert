c1567c06e30c75a236d32c3a05ebea4f995d3350
robert davis <rad316@comcast.net>
2012-07-11 11:39:46 -0400

    exoanded Chapter 1 Lesson3 Exp2
    Used contraints on Generics

--- src/Chapter1/Lesson3/Chapter1Lesson3Exp2/Chapter1Lesson3Exp2/Program.cs
+++ src/Chapter1/Lesson3/Chapter1Lesson3Exp2/Chapter1Lesson3Exp2/Program.cs
@@ -21,6 +21,10 @@ namespace Chapter1Lesson3Exp2
             Gen<double, int> g1 = new Gen<double, int>(22.6, 44);
             Console.WriteLine("{0} Generic {1}", g1._t, g1._u);
             Console.ReadLine();
+
+            GenComp<int> genComp = new GenComp<int>(3, 26);
+            Console.WriteLine("The max value is {0}",genComp.Max().ToString());
+            Console.ReadLine();
         }
     }
 
@@ -45,6 +49,27 @@ namespace Chapter1Lesson3Exp2
         {
             _t = t;
             _u = u;
+        }
+    }
+
+    class GenComp<T>
+        where T : IComparable
+    {
+        public T t1;
+        public T t2;
+
+        public GenComp(T _t1, T _t2)
+        {
+            t1 = _t1;
+            t2 = _t2;
+        }
+
+        public T Max()
+        {
+            if(t2.CompareTo(t1) < 0)
+                return t1;
+            else
+                return t2;
         }
     }
 }
