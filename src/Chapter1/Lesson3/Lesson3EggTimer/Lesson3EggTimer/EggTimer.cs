0030a2be694f9b467c98ecf7ed2db252149bcba2
robert davis <rad316@comcast.net>
2012-07-13 10:05:58 -0400

    added eggtimer [rpgram
    I used this to demonstrate events and delegates.

---
+++ src/Chapter1/Lesson3/Lesson3EggTimer/Lesson3EggTimer/EggTimer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace Lesson3EggTimer
+{
+    class EggTimer
+    {
+        Timer timer;
+        int Ticks;
+        DateTime startTime;
+
+        public double Duration { get; set; }
+
+        public event EventHandler onAlarm;
+        public delegate void EventHandler();
+
+        public EggTimer()
+        {
+            startTime = new DateTime();
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+            Duration = 10000;
+        }
+
+        //Responds to Timer Elapsed event
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Ticks++;
+            if (DateTime.Now >= startTime.AddMilliseconds(Duration))
+            {
+                timer.Enabled = false;
+                onAlarm();
+            }
+            else
+                Console.WriteLine(Ticks.ToString());
+        }
+
+        
+        public void Start()
+        {
+            Ticks=0;
+            timer.Enabled = true;
+            startTime = DateTime.Now;
+        }
+    }
+}
---
+++ src/Chapter1/Lesson3/Lesson3EggTimer/Lesson3EggTimer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson3EggTimer
+{
+    class Program
+    {
+        public static bool completed = false;
+        static void Main(string[] args)
+        {
+
+            EggTimer eggTimer = new EggTimer();
+            //eggTimer.Duration = 3000;
+            eggTimer.onAlarm += new EggTimer.EventHandler(eggTimer_onAlarm);
+
+            Console.WriteLine("Press Key to start....");
+            Console.ReadLine();
+            Console.WriteLine("Started...");
+            eggTimer.Start();
+            while (!completed)
+            {
+
+            }
+
+            Console.WriteLine("Press Key to exit...");
+            Console.ReadLine();
+
+        }
+
+        static void eggTimer_onAlarm()
+        {
+            Console.WriteLine("The timer has expired");
+            completed = true;
+        }
+
+    }
+}
