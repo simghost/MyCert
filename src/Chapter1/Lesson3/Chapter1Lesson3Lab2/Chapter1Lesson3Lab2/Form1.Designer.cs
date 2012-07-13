11f6cf6c30c75096039f880b24468a9955480cb0
robert davis <rad316@comcast.net>
2012-07-13 16:30:32 -0400

    added Chapter1 Lesson3 Lab2
    another timer event example

---
+++ src/Chapter1/Lesson3/Chapter1Lesson3Lab2/Chapter1Lesson3Lab2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Chapter1Lesson3Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblComplete = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 84);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(582, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Enabled = false;
+            this.btnExit.Location = new System.Drawing.Point(266, 155);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblComplete
+            // 
+            this.lblComplete.AutoSize = true;
+            this.lblComplete.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComplete.Location = new System.Drawing.Point(218, 21);
+            this.lblComplete.Name = "lblComplete";
+            this.lblComplete.Size = new System.Drawing.Size(171, 37);
+            this.lblComplete.TabIndex = 2;
+            this.lblComplete.Text = "Complete !";
+            this.lblComplete.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 190);
+            this.Controls.Add(this.lblComplete);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Chapter 1 Lesson 3 Lab 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblComplete;
+    }
+}
+
---
+++ src/Chapter1/Lesson3/Chapter1Lesson3Lab2/Chapter1Lesson3Lab2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Chapter1Lesson3Lab2
+{
+    public partial class Form1 : Form
+    {
+        int progress = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar.Maximum = 100;
+            progressBar.Minimum = 0;
+            progressBar.Value = 0;
+
+            timer1.Tick += new EventHandler(t_Tick);
+            timer1.Interval = 1000;
+            timer1.Start();
+
+        }
+
+        public void t_Tick(object sender, EventArgs args)
+        {
+            progressBar.Value += 10;
+            if (progressBar.Value == 100)
+            {
+                timer1.Stop();
+                lblComplete.Visible = true;
+                btnExit.Enabled = true;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
