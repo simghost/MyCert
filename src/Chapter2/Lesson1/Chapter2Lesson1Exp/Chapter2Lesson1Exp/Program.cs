3d49829050113aaa642a589d1842f616aaf8d98a
robert davis <rad316@comcast.net>
2012-07-18 10:32:56 -0400

    contined Chapter2 Lesson 1 exp

--- src/Chapter2/Lesson1/Chapter2Lesson1Exp/Chapter2Lesson1Exp/Program.cs
+++ src/Chapter2/Lesson1/Chapter2Lesson1Exp/Chapter2Lesson1Exp/Program.cs
@@ -10,10 +10,43 @@ namespace Chapter2Lesson1Exp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("reading drives\n");
             foreach (DriveInfo drive in DriveInfo.GetDrives())
             {
                 Console.WriteLine("{0} : {1}",drive.Name, drive.DriveType);
             }
+
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("reading directories\n");
+
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\");
+            foreach (DirectoryInfo item in dirInfo.GetDirectories())
+            {
+                Console.WriteLine(item.FullName);
+            }
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("read files");
+            foreach (FileInfo item in dirInfo.GetFiles())
+            {
+                Console.WriteLine(item.FullName);
+            }
+
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Create directory");
+            DirectoryInfo newDir = new DirectoryInfo(@"C:\DeleteMe");
+            if (!newDir.Exists)
+            {
+                newDir.Create();
+                Console.WriteLine("Folder created");
+            }
+            else
+                Console.WriteLine("Directory already exists.");
 
             Console.ReadLine();
         }
