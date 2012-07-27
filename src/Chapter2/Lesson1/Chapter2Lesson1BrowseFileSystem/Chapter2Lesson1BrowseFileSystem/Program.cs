982dc5ab0285f35d2c6062b957725ee5c8919fd7
robert davis <rad316@comcast.net>
2012-07-27 11:09:10 -0400

    Chapter 2 lesson 1 Ex 1

---
+++ src/Chapter2/Lesson1/Chapter2Lesson1BrowseFileSystem/Chapter2Lesson1BrowseFileSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Chapter2Lesson1BrowseFileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Drives:");
+            foreach (DriveInfo item in DriveInfo.GetDrives())
+            {
+                Console.WriteLine("{0},{1}",item.Name,item.DriveType);
+            }
+
+            Console.WriteLine("\nPress a letter to view files and folders");
+            ConsoleKeyInfo key =  Console.ReadKey();
+
+            foreach (var item in Directory.GetDirectories(key.KeyChar+":\\"))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Files:");
+
+            foreach (var item in Directory.GetFiles(key.KeyChar+":\\"))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
