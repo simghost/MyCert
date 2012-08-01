1616b8d79e94c3bd411ba33c8dcc80e7b906fb8c
robert davis <rad316@comcast.net>
2012-08-01 15:22:49 -0400

    added chapter 2 lesson 1 lab 1
    This really is lesson 2

---
+++ src/Chapter2/Lesson2/Chapter1Lesson2exp/Chapter1Lesson2exp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Chapter1Lesson2exp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader tr = File.OpenText(@"C:\windows\win.ini");
+            Console.WriteLine(tr.ReadToEnd());
+            tr.Close();
+
+            Console.ReadLine();
+
+            StreamReader sr = new StreamReader(@"C:\windows\win.ini");
+            string line;
+
+            while ((line=sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+
+            }
+
+            Console.ReadLine();
+
+            TextWriter tw = File.CreateText(@"C:\testtext.txt");
+            tw.WriteLine("This is a test line to see if this file was created properly.");
+            tw.WriteLine("This is a second line just for the heck of it.");
+            tw.Close();
+
+            FileStream fs = new FileStream(@"C:\bintest.bin",FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            for (int i = 0; i < 11; i++)
+            {
+                bw.Write(i);
+            }
+            bw.Close();
+            fs.Close();
+
+            fs = new FileStream(@"C:\bintest.bin", FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(br.ReadInt32().ToString());
+            }
+            br.Close();
+            fs.Close();
+            Console.ReadLine();
+
+           
+
+        }
+    }
+}
---
+++ src/Chapter2/Lesson2/Chapter2Lesson1Lab1/Chapter2Lesson1Lab1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Chapter2Lesson1Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            MemoryStream ms = new MemoryStream();
+            StreamWriter sw = new StreamWriter(ms);
+
+            Console.WriteLine("Enter text. Type 'quit' when finished...");
+
+            while ((text=Console.ReadLine()) != "quit")
+            {
+                sw.WriteLine(text);
+            }
+
+            sw.Flush();
+
+            FileStream fs = File.Create(@"C:\memStreamTest.txt");
+            ms.WriteTo(fs);
+
+            fs.Close();
+            sw.Close();
+            ms.Close();
+
+            Console.Clear();
+            Console.WriteLine("Entered Text:");
+                                   
+            StreamReader sr = new StreamReader(@"C:\memStreamTest.txt");
+            Console.Write(sr.ReadToEnd());
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
