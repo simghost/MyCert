c5c2fad0e1bb9fb608c2604d6fa80f696a3532ce
robert davis <rad316@comcast.net>
2012-08-02 12:02:55 -0400

    wrote Chapter 2 Lesson 2 lab 1 for a second time

--- src/Chapter2/Lesson2/Chapter2Lesson1Lab1/Chapter2Lesson1Lab1/Program.cs
+++ src/Chapter2/Lesson2/Chapter2Lesson1Lab1/Chapter2Lesson1Lab1/Program.cs
@@ -10,34 +10,40 @@ namespace Chapter2Lesson1Lab1
     {
         static void Main(string[] args)
         {
-            string text;
+            string text="";
 
             MemoryStream ms = new MemoryStream();
             StreamWriter sw = new StreamWriter(ms);
 
-            Console.WriteLine("Enter text. Type 'quit' when finished...");
-
+            Console.WriteLine("Please enter text. When complete, type 'quit'...");
             while ((text=Console.ReadLine()) != "quit")
             {
+                
                 sw.WriteLine(text);
             }
 
             sw.Flush();
 
-            FileStream fs = File.Create(@"C:\memStreamTest.txt");
-            ms.WriteTo(fs);
+            Console.Clear();
+            Console.WriteLine("Writin memory stream contents to file...");
 
+            FileStream fs = new FileStream(@"C:\memStreamTest2.txt", FileMode.Create);
+            ms.WriteTo(fs);
             fs.Close();
             sw.Close();
             ms.Close();
 
-            Console.Clear();
-            Console.WriteLine("Entered Text:");
-                                   
-            StreamReader sr = new StreamReader(@"C:\memStreamTest.txt");
-            Console.Write(sr.ReadToEnd());
-            sr.Close();
             Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("This is the co ntents of the file:..."
+                );
+            TextReader tr = File.OpenText(@"C:\memStreamTest2.txt");
+            Console.WriteLine(tr.ReadToEnd());
+            tr.Close();
+
+            Console.ReadLine();
+
         }
     }
 }
